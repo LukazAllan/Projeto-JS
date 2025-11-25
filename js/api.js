cb6626e8638915f5293fb0745d8bec01ccefa747
LukazAllan <allanlucas130@gmail.com>
2025-11-25 16:59:54 -0300

    1ª Implementação on work
    Add form input attributes and enhance message submission handling

--- js/api.js
+++ js/api.js
@@ -1,47 +1,47 @@
-function obterMensagens() {
+var form = window.document.getElementsByTagName("form")[0];
 
+function obterMensagens() {
     var retorno = [];
 
     var consulta = $.ajax({
-        url: 'https://app-p2-js-c88e9128234a.herokuapp.com/mensagens',
-        method: 'GET',
-        dataType: 'json',
-        async: false
-    }).fail(function(){
+        url: "https://app-p2-js-c88e9128234a.herokuapp.com/mensagens",
+        method: "GET",
+        dataType: "json",
+        async: false,
+    }).fail(function () {
         return retorno;
     });
 
-    consulta.done(function(data) {
+    consulta.done(function (data) {
         retorno = data;
     });
 
     return retorno;
 }
 
-function inserirMensagem(mensagem) {
-
-    /*
-
+function inserirMensagem() {
+    // var mensagem = {
+    //         nome: "nome da pessoa",
+    //         email: "email informado",
+    //         mensagem: "a mensagem informada"}
     var mensagem = {
-            nome: "nome da pessoa", 
-            email: "email informado", 
-            mensagem: "a mensagem informada"} 
-
-    */
+        nome: form.querySelector("#nome").value,
+        email: form.querySelector("#email").value,
+        mensagem: form.querySelector("#msg").value,
+    };
+    console.log(mensagem);
 
     var inserir = $.ajax({
-
-        url: 'https://app-p2-js-c88e9128234a.herokuapp.com/mensagens',
-        method: 'POST',
+        url: "https://app-p2-js-c88e9128234a.herokuapp.com/mensagens",
+        method: "POST",
         data: JSON.stringify(mensagem),
-        dataType: 'json',
+        dataType: "json",
         async: false,
-        contentType: 'application/json',
+        contentType: "application/json",
     });
 }
 
 function validarUsuario(objLoginSenha) {
-
     //email: admin@admin.com
     //senha: '1234'
 
@@ -56,20 +56,20 @@ function validarUsuario(objLoginSenha) {
     var retorno = false;
 
     var validacao = $.ajax({
-        url: 'https://app-p2-js-c88e9128234a.herokuapp.com/usuarios/validar',
-        method: 'POST',
-        dataType: 'json',
+        url: "https://app-p2-js-c88e9128234a.herokuapp.com/usuarios/validar",
+        method: "POST",
+        dataType: "json",
         async: false,
         headers: {
-            'Access-Control-Allow-Origin': '*'
-                },
-        contentType: 'application/json',
-        data: JSON.stringify(objLoginSenha)
-    }).fail(function(){
+            "Access-Control-Allow-Origin": "*",
+        },
+        contentType: "application/json",
+        data: JSON.stringify(objLoginSenha),
+    }).fail(function () {
         return retorno;
     });
 
-    validacao.done(function(data) {
+    validacao.done(function (data) {
         retorno = data;
     });
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-window.document.getElementById("enviar").addEventListener("click", () => {
+let botao = window.document.getElementById("enviar");
+botao.addEventListener("click", () => {
     console.log("Aqui se envia uma mensagem!");
+    inserirMensagem();
+});
+
+botao.addEventListener("keydown", (event) => {
+    console.log("Aqui se envia uma mensagem!");
+    if (event.key == 'Enter'){
+        inserirMensagem();
+    }
 });
