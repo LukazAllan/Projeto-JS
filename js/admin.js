be6b7672af5dd3b59b81f892ac5bf017068cb0b0
LukazAllan on Arch btw <allanlucas130@gmail.com>
2025-12-05 16:27:04 -0300

    2ª Implementação finalizada
    Update admin validation and modify page titles for clarity

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,22 @@
 let botao = window.document.getElementById("enviar");
 
+function validando(){
+    let retorno = validarUsuario();
+    if (retorno) {
+        window.location.href = "mensagens.html";
+    } else {
+        window.alert("Usuário ou senha inválidos!");
+    }
+}
+
 botao.addEventListener("click", () => {
     console.log("Aqui se valida um usuário!");
-    validarUsuario();
+    validando();
 });
 
 botao.addEventListener("keydown", (event) => {
     console.log("Aqui se valida um usuário!");
     if (event.key == 'Enter'){
-       validarUsuario();
+       validando();
     }
 });
