57f7faaed558961c37ea16d31bd2c75d471f53b5
LukazAllan on Arch btw <allanlucas130@gmail.com>
2025-12-06 13:39:24 -0300

    Refactor mensagens.js to improve message visibility handling and localStorage integration

--- js/mensagens.js
+++ js/mensagens.js
@@ -1,4 +1,19 @@
 var form = window.document.querySelector("#mensagens");
+var vistoAte = 0;
+
+function get_visto_ate(){
+    vistoAte = Number.parseInt(localStorage.getItem("vistoAte"));
+}
+function set_visto_ate(){
+    localStorage.setItem("vistoAte", vistoAte.toString());
+}
+
+function is_visto(id) {
+    if (id > vistoAte) {
+        return true;
+    }
+    return false;
+}
 
 function obterMensagens() {
     var retorno = [];
@@ -21,11 +36,20 @@ function obterMensagens() {
 
 function mostrarMensagens(mensagens) {
     for (m of mensagens) {
-        form.innerHTML =
-            form.innerHTML +
-            `<tr><td>${m['id']}</td><td>${m['nome']}</td><td>${m['email']}</td><td>${m['mensagem']}</td><td><div class="buttons">\
+        if (is_visto(m['id'])) {
+            form.innerHTML =
+                form.innerHTML +
+                `<tr><td>${m['id']}</td><td>${m['nome']}</td><td>${m['email']}</td><td>${m['mensagem']}</td><td><div class="buttons">\
        <button id="enviar" class="button">Apagar</button>\
       </div></td></tr>`;
+        } else {
+            form.innerHTML =
+            form.innerHTML +
+            `<tr class="not_seen"><td>${m['id']}</td><td>${m['nome']}</td><td>${m['email']}</td><td>${m['mensagem']}</td><td><div class="buttons">\
+       <button id="enviar" class="button">Apagar</button>\
+      </div></td></tr>`;
+        }
+
     }
 }
 
@@ -33,4 +57,11 @@ function load() {
     mostrarMensagens(obterMensagens());
 }
 
+function saveMensagens(mensagens) {
+    localStorage.setItem("mensagens", JSON.stringify(mensagens));
+}
+
+function loadMensagens() {
+    return JSON.parse(localStorage.getItem("mensagens"));
+}
 load();
