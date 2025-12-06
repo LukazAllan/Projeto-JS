2ae0f681bc997b6df4cfeb1963d5c531260f8c2d
LukazAllan on Arch btw <allanlucas130@gmail.com>
2025-12-06 14:27:46 -0300

    Refactor mensagens.js and mensagens.html to improve message handling and add update functionality

--- js/mensagens.js
+++ js/mensagens.js
@@ -1,11 +1,20 @@
 var form = window.document.querySelector("#mensagens");
-var vistoAte = 0;
+var vistoAte;
+var mensagens;
 
 function get_visto_ate(){
-    vistoAte = Number.parseInt(localStorage.getItem("vistoAte"));
+    return Number.parseInt(localStorage.getItem("vistoAte"));
 }
-function set_visto_ate(){
-    localStorage.setItem("vistoAte", vistoAte.toString());
+function set_visto_ate(inteiro){
+    localStorage.setItem("vistoAte", inteiro.toString());
+}
+
+function get_mensagens(){
+    return JSON.parse(localStorage.getItem("mensagens"));
+}
+
+function set_mensagens(mensagens){
+    localStorage.setItem("mensagens", JSON.stringify(mensagens));
 }
 
 function is_visto(id) {
@@ -14,6 +23,15 @@ function is_visto(id) {
     }
     return false;
 }
+
+document.querySelector("#atualizar").addEventListener("click", () => {
+    load();
+});
+document.querySelector("#atualizar").addEventListener("keydown", (event) => {
+    if (event.key == 'Enter'){
+        load();
+    }
+});
 
 function obterMensagens() {
     var retorno = [];
@@ -35,27 +53,57 @@ function obterMensagens() {
 }
 
 function mostrarMensagens(mensagens) {
-    for (m of mensagens) {
-        if (is_visto(m['id'])) {
+    let L = mensagens.length;
+    for (let i = 1; i <= mensagens.length; i++) {
+    // for (m of mensagens) {
+        if (is_visto(mensagens[L-i]['id'])) {
             form.innerHTML =
                 form.innerHTML +
-                `<tr><td>${m['id']}</td><td>${m['nome']}</td><td>${m['email']}</td><td>${m['mensagem']}</td><td><div class="buttons">\
+                `<tr><td>${mensagens[L-i]['id']}</td><td>${mensagens[L-i]['nome']}</td><td>${mensagens[L-i]['email']}</td><td>${mensagens[L-i]['mensagem']}</td><td><div class="buttons">\
        <button id="enviar" class="button">Apagar</button>\
       </div></td></tr>`;
         } else {
             form.innerHTML =
             form.innerHTML +
-            `<tr class="not_seen"><td>${m['id']}</td><td>${m['nome']}</td><td>${m['email']}</td><td>${m['mensagem']}</td><td><div class="buttons">\
-       <button id="enviar" class="button">Apagar</button>\
-      </div></td></tr>`;
+            `<tr class="not_seen"><td>${mensagens[L-i]['id']}</td><td>${mensagens[L-i]['nome']}\
+            </td><td>${mensagens[L-i]['email']}</td><td>${mensagens[L-i]['mensagem']}</td><td>\
+            <div class="buttons"><button id="apagar${L-i}" class="button">Apagar</button>\
+            </div></td></tr>`;
         }
+        document.querySelector(`#apagar${L-i}`).addEventListener("click", () => {
+            apagarMensagem(mensagens[L-i]['id']);
+        });
+        document.querySelector(`#apagar${L-i}`).addEventListener("keydown", (event) => {
+            if (event.key == 'Enter'){
+                apagarMensagem(mensagens[L-i]['id']);
+            }
+        });
 
     }
+
+}
+
+function clear() {
+    let N = document.querySelector("#mensagens").children.length;
+    for (let i = 0; i < N; i++) {
+        document.querySelector(`#apagar${i}`).removeEventListener("click", () => {
+            apagarMensagem(i);
+        });
+        document.querySelector(`#apagar${i}`).removeEventListener("keydown", (event) => {
+            if (event.key == 'Enter'){
+                apagarMensagem(i);
+            }
+        });
+    }
+    form.innerHTML = '';
 }
 
 function load() {
+    clear();
     mostrarMensagens(obterMensagens());
+    console.log('loaded');
 }
+
 
 function saveMensagens(mensagens) {
     localStorage.setItem("mensagens", JSON.stringify(mensagens));
@@ -64,4 +112,12 @@ function saveMensagens(mensagens) {
 function loadMensagens() {
     return JSON.parse(localStorage.getItem("mensagens"));
 }
+
+if (get_visto_ate() !== NaN) {
+    vistoAte = get_visto_ate();
+} else {
+    vistoAte = 0;
+    set_visto_ate(vistoAte);
+}
+
 load();
