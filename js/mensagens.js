834e8053fc68275efa9af511100e6ac259825068
LukazAllan <allanlucas130@gmail.com>
2025-12-08 21:10:04 -0300

    4ª Implementação finalizada

--- js/mensagens.js
+++ js/mensagens.js
@@ -4,8 +4,8 @@ var state = new State(form);
 // Listeners
 function atualizar() {
     state.atualizarMensagens();
-    state.eraseTabela();
     state.writeTabela();
+    state.putListeners();
 }
 document.querySelector("#atualizar").addEventListener("click", () => {
     atualizar();
@@ -19,8 +19,8 @@ document.querySelector("#atualizar").addEventListener("keydown", (event) => {
 function ler_todos() {
     state.vistoAte = state.mensagens[state.mensagens.length - 1]["id"];
     state.pushVistoAteLocal();
-    state.eraseTabela();
     state.writeTabela();
+    state.putListeners();
 }
 document.querySelector("#ler_todos").addEventListener("click", () => {
     ler_todos();
@@ -31,44 +31,11 @@ document.querySelector("#ler_todos").addEventListener("keydown", (event) => {
     }
 });
 
-// L = state.mensagens.length;
-// for (let i = 1; i <= L; i++) {
-//     Linha = document.querySelector(`#${state.mensagens[L - i]["id"]}`);
-//     /*Supunhetamos que temos um L == 5,
-//     i é um contador que vai de 1 a 5, e incrementando, id's diminuem
-//     no entanto, os ids das mensagens não são sequenciais.
-//     na primeira iteração, queremos o 5° elemento com id 1579, obtemos indice 4 (5 - 1 = 4)
-//     na segunda iteração, queremos o 4° elemento com id 1532, obtemos indice 3 (5 - 2 = 3)
-//     na terceira iteração, queremos o 3° elemento com id 1490, obtemos indice 2 (5 - 3 = 2)
-//     na quarta iteração, queremos o 2° elemento com id 1450, obtemos indice 1 (5 - 4 = 1)
-//     na quinta iteração, queremos o 1° elemento com id 1400, obtemos indice 0 (5 - 5 = 0)
-//     então, na elésima interação, queremos o L° elemento com id X obtemos indice (L - i)
-//     */
-
-//     document.querySelector(`#excluir${L - i}`).addEventListener("click", () => {
-//         state.excluirMensagem(L - i);
-//     });
-//     document.querySelector(`#excluir${L - i}`).addEventListener("keydown", (event) => {
-//         if (event.key == "Enter") {
-//             state.excluirMensagem(L - i);
-//         }
-//     });
-
-//     document.querySelector(`#ler${L - i}`).addEventListener("click", () => {
-//         ler(mensagens[L - i]["id"]);
-//     });
-//     document.querySelector(`#ler${L - i}`).addEventListener("keydown", (event) => {
-//         if (event.key == "Enter") {
-//             ler(mensagens[L - i]["id"]);
-//         }
-//     });
-// }
-
 // Initial
-if (state.pullMensagensLocal() !== null && state.pullMensagensLocal() !== undefined) {
+if (localStorage.getItem("mensagens") !== undefined) {
     console.debug("Fetching mensagens from local storage...");
     // state.mensagens = state.pullMensagensLocal();
-    state.set_mensagens(state.pullMensagensLocal());
+    state.pullMensagensLocal();
 } else {
     // state.mensagens = state.pullMensagensOrigin();
     console.debug("Fetching mensagens from origin...");
@@ -85,3 +52,5 @@ if (state.pullVistoAteLocal() !== null && state.pullVistoAteLocal() !== undefine
 }
 
 state.writeTabela();
+state.putListeners();
+console.debug("Initial setup complete.");
--- js/mensagens_State.js
+++ js/mensagens_State.js
@@ -9,7 +9,6 @@ State
     pullVistoAteLocal()
     pushVistoAteLocal()
     writeTabela()
-    eraseTabela()
     atualizarMensagens()
     ler(id)
 */
@@ -18,7 +17,6 @@ class State {
         this.form = form;
         this.vistoAte = 0;
         this.mensagens = [];
-        this.tabela = new Tabela(form);
         this.listeners = new Listeners();
         console.debug(this.mensagens);
     }
@@ -50,6 +48,12 @@ class State {
         return false;
     }
 
+    set_visto_ate(n) {
+        console.debug("set_visto_ate()");
+        this.vistoAte = n;
+        this.pushVistoAteLocal();
+    }
+
     obterMensagens() {
         console.debug("obterMensagens()");
         var retorno = [];
@@ -72,23 +76,26 @@ class State {
         return retorno;
     }
 
-    excluirMensagem(arrayPos) {
+    excluirMensagem(arrayPos, id) {
         console.debug("excluirMensagem()");
-        //window.alert(`Excluindo mensagem de index ${arrayPos}`);
-        fazer = window.confirm(`Tem certeza que deseja excluir a mensagem de ID ${arrayPos[id]}? Esta ação não pode ser desfeita.`);
+        console.debug(`excluirMensagem() recebeu arrayPos: ${arrayPos}, id: ${id}`);
+        var fazer = window.confirm(`Tem certeza que deseja excluir a mensagem ${arrayPos} de ID ${this.mensagens[arrayPos]["id"]}? Esta ação não pode ser desfeita.`);
+        // this.form.removeChild(this.form.children[this.form.childElementCount - 1 - arrayPos]);
         if (fazer) {
-            this.mensagens.splice(arrayPos, 1);
-            document.querySelector(`#excluir${arrayPos}`).removeEventListener("click", () => {
-                excluirMensagem(arrayPos);
-            });
-            document.querySelector(`#excluir${arrayPos}`).removeEventListener("keydown", (event) => {
-                if (event.key == "Enter") {
-                    excluirMensagem(arrayPos);
-                }
-            });
+            this.set_mensagens(this.mensagens.filter((mensagem) => mensagem["id"] != id));
+            //console.log(this.mensagens.splice(arrayPos, 1));
         }
-        this.eraseTabela();
+        this.pushMensagensLocal();
         this.writeTabela();
+        this.putListeners();
+        console.debug("excluirMensagem() => Exit with code 0");
+        // this.set_mensagens(as_mensagens);
+        // this.pushMensagensLocal();
+        // this.eraseTabela();
+        // this.writeTabela();
+        // Essa parte abaixo ARRANCA a mensagem da tabela visualmente
+        //this.form.removeChild(this.form.children[arrayPos]);
+        //this.form.removeChild(this.form.children[this.form.childElementCount - 1 - arrayPos]);
     }
 
     pullMensagensOrigin() {
@@ -121,7 +128,7 @@ class State {
 
     writeTabela() {
         console.debug("writeTabela()");
-        this.tabela.exibirNaTabela(this.form, this.obterMensagens(), (id) => {
+        this.exibirNaTabela(this.form, this.get_mensagens(), (id) => {
             if (id <= this.vistoAte) {
                 return true;
             }
@@ -131,27 +138,113 @@ class State {
         // this.obterMensagens()
     }
 
-    eraseTabela() {
-        console.debug("eraseTabela()");
-        this.tabela.limpaTabela(this.form);
-    }
-
     atualizarMensagens() {
         console.debug("atualizarMensagens()");
-        let O = this.obterMensagens();
-        O.forEach((element) => {
-            if (this.is_visto(element["id"])) {
-                this.mensagens.push(element);
+        let pulled = this.obterMensagens();
+        for (let n of pulled) {
+            if (this.is_visto(n["id"] + 1)) {
+                continue;
+            } else {
+                this.mensagens.push(n);
             }
-        });
+        }
+        this.pushMensagensLocal();
     }
 
     ler(id) {
         console.debug("ler()");
-        if (!is_visto(id)) {
+        var fazer = window.confirm(`Tem certeza que leu até a mensagem de ID ${id}? Esta ação não pode ser desfeita.`);
+        if (id > this.vistoAte) {
             console.log(`Lendo até mensagem de id ${id}`);
-            this.set_visto_ate(id.toString());
-            this.vistoAte = id;
+            this.set_visto_ate(id);
         }
+        this.writeTabela();
+        this.putListeners();
+        console.debug("ler() => Exit with code 0");
+    }
+
+    putListeners() {
+        for (let i = 0; i < state.mensagens.length; i++) {
+            console.debug(`putListeners()`);
+            //Linha = document.querySelector(`#${state.mensagens[i]["id"]}`);
+
+            document.querySelector(`#excluir${i}`).addEventListener("click", () => {
+                state.excluirMensagem(i, state.mensagens[i]["id"]);
+            });
+            document.querySelector(`#excluir${i}`).addEventListener("keydown", (event) => {
+                if (event.key == "Enter") {
+                    state.excluirMensagem(i, state.mensagens[i]["id"]);
+                }
+            });
+
+            document.querySelector(`#ler${i}`).addEventListener("click", () => {
+                state.ler(this.mensagens[i]["id"]);
+            });
+            document.querySelector(`#ler${i}`).addEventListener("keydown", (event) => {
+                if (event.key == "Enter") {
+                    state.ler(this.mensagens[i]["id"]);
+                }
+            });
+        }
+    }
+
+    constructMensagemLinha(mensagem, n) {
+        // console.log('exibirNaTela()');
+        var Linha = document.createElement("tr");
+        var id = document.createElement("td");
+        var nome = document.createElement("td");
+        var email = document.createElement("td");
+        var msg = document.createElement("td");
+
+        if (!this.is_visto(mensagem["id"])) {
+            // is_visto
+            Linha.classList.add("not_seen");
+        }
+
+        Linha.id = n.toString();
+        id.innerText = mensagem["id"];
+        nome.innerText = mensagem["nome"];
+        email.innerText = mensagem["email"];
+        msg.innerText = mensagem["mensagem"];
+
+        var botoes = document.createElement("td");
+        var botao_excluir = document.createElement("button");
+        botao_excluir.classList.add("button");
+        botao_excluir.id = `excluir${n}`;
+        botao_excluir.innerText = "❌";
+        var botao_ler = document.createElement("button");
+        botao_ler.classList.add("button");
+        botao_ler.id = `ler${n}`;
+        botao_ler.innerText = "📥";
+        botoes.appendChild(botao_excluir);
+        botoes.appendChild(botao_ler);
+
+        Linha.appendChild(id);
+        Linha.appendChild(nome);
+        Linha.appendChild(email);
+        Linha.appendChild(msg);
+        Linha.appendChild(botoes);
+        console.log("constructMensagemLinha() => return");
+        return Linha;
+    }
+
+    limpaTabela() {
+        console.log("limpaTabela()");
+        while (this.form.firstChild) {
+            this.form.removeChild(this.form.firstChild);
+        }
+        //this.form.innerHTML = "";
+        console.log("Tabela limpa com sucesso!");
+    }
+
+    exibirNaTabela(form, func) {
+        console.log("exibirNaTabela()");
+        this.limpaTabela(form);
+        let Linha;
+        for (let i = this.mensagens.length - 1; i >= 0; i--) {
+            Linha = this.constructMensagemLinha(this.mensagens[i], i);
+            form.appendChild(Linha);
+        }
+        console.log("Mensagens exibidas com sucesso!");
     }
 }
